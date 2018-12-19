11c34d38f71fd88f764dd86266a06bf7899bbc0c
bhatnagarm <mitul.bhatnagar2@virginaustralia.com>
2018-12-19 11:27:53 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect = {
+  perimeter: (x,y) => (2*(x+y)),
+  area: (x,y) =>(x*y)
+};
+
+function solveRect(l,b) {
+  console.log("Solving for rectangle with l= " + l + " and b= " + b);
+
+  if (l <=0 || b<= 0){
+    console.log("Rectangle dimentions should be greater then 0");
+  } else {
+    console.log("The area of the rectagle is " + rect.area(l,b));
+    console.log("The perameter of the rectangle is " + rect.perimeter(l,b));
+
+  }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
